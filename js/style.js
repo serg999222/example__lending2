3d3da5b92717ccf925050e3a8c28cd1897007bb6
serg999222 <gazenkos935@gmail.com>
2022-06-17 13:45:15 +0300

    headerReady

---
+++ js/style.js
@@ -0,0 +1,8 @@
+//==Burger menu=====================================//
+document.querySelector('.header__burger').onclick = function(){
+	document.querySelector('.header__menu').classList.toggle('active')
+	document.querySelector('.header__burger').classList.toggle('active')
+}
+
+//==slider1====================================//
+

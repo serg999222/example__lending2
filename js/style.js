1e9b0d75ad6186ebb7b57e9afd90232fb8f987dc
serg999222 <gazenkos935@gmail.com>
2022-06-23 10:59:44 +0300

    start block2

--- js/style.js
+++ js/style.js
@@ -7,73 +7,31 @@ document.querySelector('.header__burger').onclick = function(){
 //==slider1====================================//
 
 
-
-// function currentSlide(n){
-// 	showSlides(slideIndex = n)
-// }
-
-// function showSlides(n){
-// 	//let i
-// 	let slides = document.getElementsByClassName("slider1__fade")
-// 	// let dots = document.getElementsByClassName("dot")
-
-// 	if (n > slides.length){
-// 		slideIndex = 1
-// 	}
-// 	if  (n < 1 ){
-// 		slideIndex = slides.length
-// 	}
-
-// 	for (let i = 0; i < slides.length; i++){
-// 		slides[i].style.display='none'
-// 	}
-
-// 	// for (i = 0; i < dots.length; i++){
-// 	// 	dots[i].className = dots[i].className.replace('active',"")
-// 	// }
-// 	slides[slideIndex+1].style.display='block'
-// 	// dots[slideIndex-1].className +='active'
-// }
-
-// var slideIndex = 1
-// showSlides(slideIndex)
-
-// function plusSlides(n){
-// 	showSlides(slideIndex += n)	
-// }
-// let current = 0
-// let allSlides = document.querySelectorAll('.slider1__image')
-// function showSlides(){
-// for (let i = 0; i < allSlides.length; i++){
-// 	allSlides[i].classList.add('opacity0')
-// 	}
-// 	allSlides[current].classList.remove('opacity0')
-
-// }
-// function plusSlides1(){
-// 	if (current -1 == -1){
-// 		current = allSlides.length -1
-// 	}
-// 	else{
-// 		current--
-// 	}
-// 	showSlides()
-// }
-//=================================new trying==========================//
 let current = 0
 let allSlides = document.querySelectorAll('.slider1__image')
 let nextSlide = document.getElementById('right').onclick = fRight
 let prevSlide = document.getElementById('left').onclick = fleft
-
+let buttomsRadius = document.querySelectorAll('.dot')
+buttomsRadius[current].classList.add('actyveButt')
+for (let i = 0; i < allSlides.length; i++){
+	allSlides[i].classList.add('opacity0')
+}
+allSlides[0].classList.remove('opacity0')
+ current+=1
 function fRight(){
 	for (let i = 0; i < allSlides.length; i++){
 		allSlides[i].classList.add('opacity0')
 	}
 	allSlides[current].classList.remove('opacity0')
+	for (let i = 0; i < allSlides.length; i++){
+		buttomsRadius[i].classList.remove('actyveButt')
+	}
+	buttomsRadius[current].classList.add('actyveButt')
 	current++
 	if (current >= allSlides.length){
 		return current = 0
 	}
+
 }
 
 function fleft(){
@@ -85,11 +43,19 @@ function fleft(){
 	if (current < 0){
 		return current = allSlides.length -1
 	}
+	// for (let i = 0; i < allSlides.length; i++){
+	// 	buttomsRadius[i].classList.remove('actyveButt')
+	// }
+	// buttomsRadius[current].classList.add('actyveButt')
+	// current--
+	// if (current < 0){
+	// 	return current = allSlides.length -1
+	// }
 }
 
 function currentSlide(n){
 	let radialActyve = document.querySelectorAll('.actyveButtoms')
-	let buttomsRadius = document.querySelectorAll('.dot')
+	//let buttomsRadius = document.querySelectorAll('.dot')
 	for (let i = 0; i < allSlides.length; i++){
 		allSlides[i].classList.add('opacity0')
 	}
@@ -102,4 +68,3 @@ function currentSlide(n){
 	// radialActyve[current].classList.add('radialActyve')
 
 }
-//console.log(nextSlide)

8320271790eef0c7247c6b059376faa53486c41c
serg999222 <gazenkos935@gmail.com>
2022-07-20 11:44:25 +0300

    finish my work

--- js/style.js
+++ js/style.js
@@ -3,8 +3,6 @@ document.querySelector('.header__burger').onclick = function(){
 	document.querySelector('.header__menu').classList.toggle('active')
 	document.querySelector('.header__burger').classList.toggle('active')
 	document.querySelector('.header__logo').classList.toggle('active')
- //	document.querySelector('.main').classList.toggle('active')
-	// document.querySelector('.footer').classList.toggle('active')
 	document.querySelector('.header__search').classList.toggle('active')
 	document.querySelector('body').classList.toggle('active')
 }
